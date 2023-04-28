27e8eeca1d3f36df83bbdc2a999622748cf273f7
Richard Large <richardlarge88@gmail.com>
2023-04-28 19:52:30 +0100

    Fix user listener using local ip addr

--- df-downloader-ui/src/store/user/user.listener.ts
+++ df-downloader-ui/src/store/user/user.listener.ts
@@ -2,7 +2,8 @@ import { UserInfo } from "df-downloader-common";
 import { AppStartListening } from "../listener";
 import { queryUserInfo } from "./user.actions";
 import { addFetchListener } from "../utils";
+import { API_URL } from "../../config";
 
 export const startListeningUserInfo = (startListening: AppStartListening) => {
-  addFetchListener(startListening, queryUserInfo, UserInfo, () => ["http://127.0.0.1:44556/api/user"]);
+  addFetchListener(startListening, queryUserInfo, UserInfo, () => [`${API_URL}/user`]);
 };
