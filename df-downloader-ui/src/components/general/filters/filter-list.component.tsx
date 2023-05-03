da78fed6bb626dc09a4cbb550c0dab9e8d0dc333
Richard Large <richardlarge88@gmail.com>
2023-05-03 06:58:36 +0100

    Filters are now accordians

--- df-downloader-ui/src/components/general/filters/filter-list.component.tsx
+++ df-downloader-ui/src/components/general/filters/filter-list.component.tsx
@@ -1,8 +1,9 @@
-import { Button, Divider, Stack } from "@mui/material";
+import { Accordion, AccordionDetails, AccordionSummary, Button, Divider, Stack } from "@mui/material";
 import { Fragment } from "react";
 import { FilterItemField } from "./filter-item-field.component";
 import { useFieldArray, useFormContext } from "react-hook-form";
 import { formFieldBorder } from "../../../utils/props";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 export type FilterListProps = {
   type: "exclude" | "include";
@@ -18,10 +19,19 @@ export const FilterList = ({ type }: FilterListProps) => {
   return (
     <Fragment>
       {fields.map((field, index) => (
-        <Stack sx={{ ...formFieldBorder, gap: 1 }} key={field.id}>
+        <Fragment>
           {index > 0 && <Divider>OR</Divider>}
-          <FilterItemField parentFieldName={`${fieldArrayName}.${index}`} remove={() => remove(index)} />
-        </Stack>
+          <Accordion>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              {filterName} Filter {index + 1}
+            </AccordionSummary>
+            <AccordionDetails>
+              <Stack sx={{ ...formFieldBorder, gap: 1 }} key={field.id}>
+                <FilterItemField parentFieldName={`${fieldArrayName}.${index}`} remove={() => remove(index)} />
+              </Stack>
+            </AccordionDetails>
+          </Accordion>
+        </Fragment>
       ))}
       <Button onClick={() => append({})}>Add {filterName} Filter</Button>
     </Fragment>
